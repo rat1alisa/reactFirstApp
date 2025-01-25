e486a06cc1a508a8a0846fe72b02be346e54d8bd
Alice <alekseevaalisa763@gmail.com>
2025-01-25 18:05:23 +0300

    not done

--- reactApp/src/components/HomePage/HomePage.jsx
+++ reactApp/src/components/HomePage/HomePage.jsx
@@ -7,10 +7,31 @@ import Form from "../common/MyForm/MyForm";
 
 function Home() {
     return(
-        <div className="homePageBlock">
-            <h2>Home page</h2>
-            <Form />
-        </div>
+        <div className="card">
+  <div className="container">
+    <div className="cloud front">
+      <span className="left-front"></span>
+      <span className="right-front"></span>
+    </div>
+    <span className="sun sunshine"></span>
+    <span className="sun"></span>
+    <div className="cloud back">
+      <span className="left-back"></span>
+      <span className="right-back"></span>
+    </div>
+  </div>
+
+  <div className="card-header">
+    <span>Messadine, Susah Tunisia</span>
+    <span>March 13</span>
+  </div>
+
+  <span className="temp">23°</span>
+
+  <div className="temp-scale">
+    <span>Celcius</span>
+  </div>
+</div>
     )
 }
 
--- reactApp/src/components/common/Header/Header.jsx
+++ reactApp/src/components/common/Header/Header.jsx
@@ -5,8 +5,9 @@ import "./Header.css";
 const Header = () => {
   return (
 <header className="Header">
-<NavLink to="/" end >Home Page</NavLink>
-<NavLink to="/counter">Counter</NavLink>
+<img src="https://cdn.iconscout.com/icon/free/png-256/free-apple-notes-icon-download-in-svg-png-gif-file-formats--logo-note-apps-pack-user-interface-icons-493153.png?f=webp&w=256" className="logo"/>
+<NavLink className="link" to="/" end >Home Page</NavLink>
+<NavLink className="link" to="/counter">Counter</NavLink>
 </header>
   );
 };
